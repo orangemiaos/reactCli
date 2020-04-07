de6fe408062c4de38f8e4db508cf55dff280476f
多肉张萌 <1229968688@qq.com>
2020-04-07 16:21:34 +0800

    'antd-demo'

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+// UI
+// import {Button} from "antd";
+// import 'antd/dist/antd.css';
+import Login from './pages/login/login.jsx';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Login/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './login.less';
+
+export default () => {
+    return (
+        <div className='login'>
+            <header className='login-header'>我是login</header>
+            <section className='login-content'></section>
+        </div>
+    )
+}
